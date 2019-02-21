4d5008b408f4624fbb80b9c43895c4b6d18d6c9c
ltoddy <taoliu0509@gmail.com>
2019-02-21 20:23:22 +0800

    match the same lone pattern and text

--- src/engine.rs
+++ src/engine.rs
@@ -1,11 +1,5 @@
 #[doc("单个字符的匹配")]
-pub fn match_one<S>(pattern: S, text: S) -> bool
-where
-    S: Into<String>,
-{
-    let pattern = pattern.into();
-    let text = text.into();
-
+pub fn match_one(pattern: &[char], text: &[char]) -> bool {
     if pattern.is_empty() {
         // 任何text都可匹配空的pattern
         return true;
@@ -16,23 +10,37 @@ where
         return false;
     }
 
-    if pattern == "." {
+    if pattern == ['.'] {
         return true;
     }
 
     pattern == text
 }
 
+#[doc("相同长度的字符串匹配,(由于match是关键字,这里用matches命名)")]
+pub fn matches(pattern: &[char], text: &[char]) -> bool {
+    if pattern.is_empty() {
+        true
+    } else {
+        match_one(&pattern[..0], &text[..0]) && matches(&pattern[1..], &text[1..])
+    }
+}
+
 #[cfg(test)]
 pub mod test {
-    use super::match_one;
+    use super::{match_one, matches};
 
     #[test]
     pub fn test_match_one() {
-        assert_eq!(match_one("a", "a"), true);
-        assert_eq!(match_one(".", "z"), true);
-        assert_eq!(match_one("", "g"), true);
-        assert_eq!(match_one("a", "b"), false);
-        assert_eq!(match_one("p", ""), false);
+        assert_eq!(match_one(&['a'], &['a']), true);
+        assert_eq!(match_one(&['.'], &['z']), true);
+        assert_eq!(match_one(&[], &['g']), true);
+        assert_eq!(match_one(&['a'], &['b']), false);
+        assert_eq!(match_one(&['p'], &[]), false);
+    }
+
+    #[test]
+    pub fn test_matches() {
+        assert_eq!(matches(&['a', '.', 'c'], &['a', 'b', 'c']), true);
     }
 }
